398bda87a59c460c3a9bc239d701946f29ad6693
Darío Clavijo <clavijodario@gmail.com>
2025-04-13 00:49:48 -0300

    somsuk attack

---
+++ src/RsaCtfTool/attacks/single_key/somsuk.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from RsaCtfTool.attacks.abstract_attack import AbstractAttack
+from RsaCtfTool.lib.keys_wrapper import PrivateKey
+from RsaCtfTool.lib.exceptions import FactorizationError
+from RsaCtfTool.lib.algos import somsuk
+
+
+class Attack(AbstractAttack):
+    def __init__(self, timeout=60):
+        super().__init__(timeout)
+        self.speed = AbstractAttack.speed_enum["medium"]
+
+    def attack(self, publickey, cipher=[], progress=True):
+        """Run somsuk attack with a timeout"""
+        try:
+            r = somsuk(publickey.n)
+            publickey.p, publickey.q = r
+
+        except FactorizationError:
+            self.logger.error("N should not be a 4k+2 number...")
+            return None, None
+
+        if publickey.p is not None and publickey.q is not None:
+            try:
+                priv_key = PrivateKey(
+                    n=publickey.n,
+                    p=int(publickey.p),
+                    q=int(publickey.q),
+                    e=int(publickey.e),
+                )
+                return priv_key, None
+            except ValueError:
+                return None, None
+
+        return None, None
+
+    def test(self):
+        from RsaCtfTool.lib.keys_wrapper import PublicKey
+
+        key_data = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCG6ZYBPnfEFpkADglB1IDARrL3
+Gk+Vs1CsGk1CY3KSPYpFYdlvv7AkBZWQcgGtMiXPbt7X3gLZHDhv+sKAty0Plcrn
+H0Lr4NPtrqznzqMZX6MsHGCA2Q74U9Bt1Fcskrn4MQu8DGNaXiaVJRF1EDCmWQgW
+VU52MDG8uzHj8RnGXwIDAQAB
+-----END PUBLIC KEY-----"""
+        result = self.attack(PublicKey(key_data), progress=False)
+        return result != (None, None)
